22517dcd0ab67f83075cdc95e3c4fb0529ed1c84
bita-hdt <bita.hdt@gmail.com>
2025-06-02 23:45:10 +0330

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
@@ -13,8 +12,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: isProd ? "/bita-hdt.github.io/" : "",
-  basePath: isProd ? "/bita-hdt.github.io" : "",
 };
 
 export default nextConfig;
